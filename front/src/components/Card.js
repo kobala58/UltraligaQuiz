633b8c4b5f6d06691276f04c0e476160ab9e5ed1
Kobala <bartlomiej.koba@gmail.com>
2023-04-08 17:03:54 +0200

    MVP reached

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -1,31 +1,49 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import Popup from 'reactjs-popup';
 
-function hide_nickname(nick) {
+
+
+function hide_nickname(to_hide) {
   var hide = '';
-  console.log(nick)
-  console.log(nick.length)
-  for (let i = 0; i < nick.length; i++){
+  for (let i = 0; i < to_hide.length; i++){
     hide += '_';
   }
-  console.log(hide)
   return(hide);
 }
 
+
 export default function BasicCard(props) {
+
+  const [letter_rev, setLetterRev] = useState(0);
+  const [nick, setNick] = useState(hide_nickname(props.name));
+  
+  function letter_reveal() {
+    if (props.guessed == true){
+      return 0;
+    }
+    console.log(props.name[letter_rev]);
+    let copy = nick.split('');
+    copy[letter_rev] = props.name[letter_rev];
+    setNick(copy.join(''));
+    setLetterRev(letter_rev+1);
+
+  } 
+
   return (
-    <Card sx={3}>
+    <Card sx={3} style={{backgroundColor: props.guessed ? "lightgreen":"gray"}}>
+      
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           {props.country}
         </Typography>
         <Typography variant="h5" component="div">
-          {hide_nickname(props.name)}
+          {props.guessed ? props.name : nick}
         </Typography>
         <Typography sx={{ mb: 3 }} color="text.secondary">
           {props.position}
@@ -37,7 +55,7 @@ export default function BasicCard(props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Odkryj literę</Button>
+        <Button size="small" onClick={letter_reveal}>Odkryj literę</Button>
       </CardActions>
     </Card>
   );
--- front/src/screens/HomeScreem.js
+++ front/src/screens/HomeScreem.js
@@ -1,53 +1,80 @@
 import Grid from '@mui/material/Grid';
 import OutlinedCard from '../components/Card';
-import GuessingField from '../components/GuessConsole';
 import Box from "@mui/material/Box";
-import TextField from '@mui/material/TextField';
 import TitleRender from '../components/Title';
+import { useState } from 'react';
+import AlertDialogSlide from '../components/Popup';
 
+function hide_nickname(to_hide) {
+  var hide = '';
+  for (let i = 0; i < to_hide.length; i++){
+    hide += '_';
+  }
+  return(hide);
+}
 export default function HomeScreen() {
   const data = {"Name":"Ultraliga Season 1","Team":"Illuminar Gaming","Year":2019,
-    "Player":["Raven ","CrAzY ","Kashtelan","Puki Style","delord"],
+    "Player":["Raven","CrAzY","Kashtelan","Puki Style","delord"],
     "Role":["Top","Jungle","Mid","Bot","Support"],
     "Flag":["Poland","Poland","Poland","Poland","Poland"]};
   
+  const [guesed, changeGuessed] = useState([false, false, false, false, false]); // check_hits
+  const [answer, setAnswer] = useState(''); // save ansver
+  const [guesed_count, setGuessedCount] = useState(0); // count guessed players
   
+  function handleChange (e) {
+    console.log(e.target.value.trim());
+    setAnswer(e.target.value);
+    let idx = data.Player.indexOf(e.target.value.trim());
+    if (idx != -1 && guesed[idx] == false) {
+      let copy = guesed;
+      copy[idx] = !copy[idx];
+      changeGuessed(copy);
+      setAnswer('');
+      setGuessedCount(guesed_count + 1);
+      
+      if (guesed_count == data.Player.length){
+        <AlertDialogSlide/>
+      }
+    }
+  }
 
   return (
   <div>
+      <Header/>
       <TitleRender />
     <div>
         <Grid container spacing={{ xs: 2, md: 1 }} columns={{ xs: 4, sm: 2, md: 12 }}
         justifyContent="space-evenly" alignItems="center">
           {
           data.Player.map((value, i) => (
-          <Grid item key = {value} sm={5} md={2.2} lg={5} xl={4}>
+          <Grid item key = {i} sm={5} md={2.2} lg={5} xl={4}>
               <OutlinedCard
               name = {value}
               position = {data.Role[i]}
               country = {data.Flag[i]}
               year = {data.Year}
               team = {data.Team}
+              guessed = {guesed[i]}
+              hided = {hide_nickname(value)}
               />
           </Grid>
           ))}
         </Grid>
+          {guesed_count == 5 && <AlertDialogSlide/>}
         <Box display="flex"
               justifyContent="center"
               alignItems="center"
               minHeight="30vh">
-          {/* {"Podaj nick gracza"}  */}
-          <form onSubmit={e => {
-            e.preventDefault();
-        alert('Nick:');
-        }}>
-        <input />
-        <button>Send</button>
-        </form>
-          
+
+          <form onSubmit={e => { e.preventDefault()}}>
+            <input 
+              value = {answer}
+              onChange = {handleChange}
+            />
+          </form>        
         </Box>
-</div>
-  </div>
-      
+    </div>
+  </div>    
   )
 }
